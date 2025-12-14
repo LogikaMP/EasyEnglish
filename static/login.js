db4f4347205d2ec1712be16929fbe6ba99e5b713
NaviIluha <melailuha715@gmail.com>
2025-12-14 10:29:16 +0000

    2

---
+++ static/login.js
@@ -0,0 +1,19 @@
+let all_users
+
+fetch("/static/data/user.json").then(function(res){
+    return res.json()
+}).then(function(data){
+    all_users = data
+})
+
+let btn = document.querySelector(".btn-login")
+btn.addEventListener("click",function(){
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    for(let user in all_users){
+        if(email == user && password == all_users[user]){
+            // додати анімацію ато бабайка вкраде
+            window.location.assign("/cabinet")
+        }
+    }
+})
