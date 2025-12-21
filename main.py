a751d4d13c0ddcc297b74382eee6751d15f85b6f
DETROMIN27 <94011680+DETROMIN27@users.noreply.github.com>
2025-12-21 14:47:24 +0200

    main

--- main.py
+++ main.py
@@ -13,28 +13,7 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-<<<<<<< HEAD
-    if request.method == ('POST'):
-        new_user = request.get_json
-        with open('/static/data/user.json', 'r')as f:
-            all_users = json.load(f)
-        all_users.update(new_user)
-        with open('/static/data/user.json', 'w')as f:
-            json.dump(all_users, f)
-        return 'ok'
-=======
-    if request.method == "POST":
-        new_user = request.get_json()
-        with open("static/data/user.json", "r")as f:
-            all_users = json.load(f)
-        all_users.update(new_user)
 
-        with open("static/data/user.json", "w",)as f:
-            json.dump(all_users, f)
-        return "ok"
-
-
->>>>>>> 967d631ab4fc2f14a033cac7736fb788ce240b6a
     return render_template('register.html')
 
 @app.route("/cabinet")
