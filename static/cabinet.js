b399af1e4c6b624f65d4c6e57ff2a62d88092e77
Maria Panteleichuk <varvarich65@gmail.com>
2026-01-07 17:59:10 +0200

    chceck error

--- static/cabinet.js
+++ static/cabinet.js
@@ -1,3 +1,6 @@
+let progress = {}
+let tasks = {}
+
 let  cabinet_user =localStorage.getItem("user")
 document.querySelector(".user-name").innerHTML = cabinet_user
 let btns = document.querySelectorAll(".menu-item")
@@ -12,14 +15,31 @@ for(let btn  of btns) {
         this.style.background = "#0077b6"
         let name = this.id
         let file = "/static/cards/" + name + ".html" 
+        let file_tasks = "/static/data/" + name + ".json"
+        
+        fetch(file_tasks).then(function(res){
+            return res.json()
+
+        }).then(function(data){
+            tasks = data[name]
+        })
+
+        fetch("/static/data/progress.json").then(function(res){
+            return res.json()
+
+        }).then(function(data){
+            progress = data
+        })
+
         fetch(file).then(function(res){
             return res.text()
         }).then(function(html){
+
             document.querySelector(".content-area").innerHTML = html
             let script = document.createElement("script")
             script.src = "/static/" + name + ".js"
             script.defer = true 
-            document.body.appendChild(script)
+            document.querySelector(".content-area").appendChild(script)
         })
     })
 }
