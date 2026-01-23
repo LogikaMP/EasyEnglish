926947a649d2372a66ede921e0ab482951f26d2e
Maria Panteleichuk <varvarich65@gmail.com>
2026-01-23 14:27:26 +0200

    check class work 18\01

--- static/verbs.js
+++ static/verbs.js
@@ -10,7 +10,7 @@ function load_verbs(){
     let task_verbs = ""
     function load_task(){
         document.querySelector(".verbs").innerHTML = tasks[index]["verb"]
-        document.querySelector(".verbs.tense").innerHTML = tasks[index]["tense"]
+        document.querySelector(".verbs.tense").innerHTML = tasks[index]["Tense"]
         task_verbs = [tasks[index]["I"],tasks[index]["You"],
             tasks[index]["He/She/It"],tasks[index]["He/She/It"],tasks[index]["He/She/It"],
             tasks[index]["We"],tasks[index]["They"]]
@@ -18,28 +18,44 @@ function load_verbs(){
     load_task()
     
     document.querySelector(".test.button.primary.verbs").addEventListener("click", function(){
-        let my_ans = document.querySelectorAll(".test.input.verbs")
-        let result = ''
-        let right_ans = document.querySelector(".verbs.sentence")
-        if(my_ans.value == tasks[index]["en"]){
-            result = "👍"
-            progress[cabinet_user]["verbs"][0]++
+        let my_ans = document.querySelectorAll(".test-input.verbs")
+        let r_ans = document.querySelectorAll(".right-answers")
+        let result = 0
+        let color = ""
+        for(let i=0;i<task_verbs.length;i++){
+            if(my_ans[i].value == task_verbs[i]){
+                result ++
+                color = "green"
+            }else{
+                color ="red"
+                my_ans[i].style.textDecoration = "line-through" 
+                r_ans[i].innerHTML+= "<div>" + task_verbs[i] +"</div>"
+            }
+            my_ans[i].style.backgroundColor = color
+            my_ans[i].style.color ="white"
+        }
+        if (result == 7){
+            progress[cabinet_user]["verbs"][0] ++
         }else{
-            result = "👎"
             progress[cabinet_user]["verbs"][1]++
         }
         progress[cabinet_user]["verbs"][2]++
-        right_ans.innerHTML = result + tasks[index]["en"]+"<br>"+tasks[index]["ua"]
+       
         setTimeout(function(){
-            right_ans.innerHTML = ""
-            my_ans.value = ""
+            for(let i=0;i<task_verbs.length;i++){
+                my_ans[i].style.backgroundColor = "#90e0ef"
+                my_ans[i].style.color ="#03045e"
+                my_ans[i].value = ""
+                my_ans[i].style.textDecoration = "none"
+                r_ans[i].innerHTML = ""
+            }
             index++
             if (index >= tasks.length){
                 index = 0
             }
             update_progress()
             load_task()
-        }, 1000)
+        }, 3000)
     })
 }
 load_verbs()
