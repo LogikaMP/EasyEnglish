7d08ab3e550d7af64701eb387161f07d398258ab
DETROMIN27 <94011680+DETROMIN27@users.noreply.github.com>
2026-01-31 21:02:44 +0200

    a

--- static/login.js
+++ static/login.js
@@ -1,19 +1,29 @@
+let all_user;
+
 fetch("/static/data/user.json")
     .then(res => res.json())
-    .then(users => {
-
-        document.querySelector(".login-form").addEventListener("submit", function(e){
-            e.preventDefault();
-
-            let email = document.getElementById("email").value;
-            let password = document.getElementById("password").value;
-
-            if(users[email] && users[email] === password){
-                localStorage.setItem("user", email);
-                window.location.href = "/cabinet";
-            } else {
-                document.querySelector(".error").innerText = "Перевірте логін і пароль";
-            }
-        });
-
+    .then(data => {
+        all_user = data;
     });
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.querySelector(".btn-login");
+
+    btn.addEventListener("click", function () {
+        let email = document.getElementById("email").value.trim();
+        let password = document.getElementById("password").value.trim();
+
+        if (all_user[email] && all_user[email] === password) {
+            localStorage.setItem("user", email);
+            window.location.assign("/cabinet");
+        } else {
+            document.querySelector(".error").innerText = "Перевірте логін і пароль";
+        }
+    });
+
+    // поява форми
+    setTimeout(() => {
+        document.querySelector(".logo-form").classList.add("show");
+    }, 200);
+});
